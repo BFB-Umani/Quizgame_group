95cfb022027a5b61809cc1ee41a1aab59d5c1136
MarioSamperi75 <mariosss@libero.it>
2019-11-11 13:24:24 +0100

    mario test

--- src/com/quizgame/QuizClient.java
+++ src/com/quizgame/QuizClient.java
@@ -3,5 +3,6 @@ package com.quizgame;
 public class QuizClient {
     public static void main(String[] args) {
         System.out.println("development");
+        //mario funkar igen (kanske)
     }
 }

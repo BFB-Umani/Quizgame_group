577b623625db759beab2e4c08da6864ba7270292
Kening Fohlin <kening.fohlin@gmail.com>
2019-11-14 10:52:35 +0100

    first commit frontend javafx

--- src/com/quizgame/QuizProtocol.java
+++ src/com/quizgame/QuizProtocol.java
@@ -1,4 +1,29 @@
 package com.quizgame;
 
+import com.quizgame.Controller.QuizController;
+import com.quizgame.view.QuizView;
+
 public class QuizProtocol {
+
+    private static final int WAITING = 0;
+    private static final int CHOOSINGANSWER = 1;
+    private static final int ANOTHER = 2;
+
+    private int state = WAITING;
+    private QuizController quizController;
+    private QuizView quizView;
+
+    public QuizProtocol(QuizController quizController) {
+        this.quizController = quizController;
+    }
+
+    public void result(){
+        if(state == WAITING){
+            quizController.start();
+            state = CHOOSINGANSWER;
+        }else if(state == CHOOSINGANSWER){
+
+
+        }
+    }
 }

2d6d2f91456f2c5625dc5052431df834004c61b2
Jessie Eurenius <jessie930818@gmail.com>
2019-11-27 13:19:22 +0100

    test

--- src/com/quizgame/client/ServerConnection.java
+++ src/com/quizgame/client/ServerConnection.java
@@ -20,8 +20,11 @@ public class ServerConnection extends Thread {
     private QuizClient quizClient;
     private List<QuizItem> questions;
     private SetNameObject setNameObject = new SetNameObject();
+    private List<String> threeSubjects;
 
-
+    public List<String> getThreeSubjects() {
+        return threeSubjects;
+    }
 
     public ServerConnection(QuizClient quizClient){
         this.quizClient = quizClient;
@@ -76,8 +79,9 @@ public class ServerConnection extends Thread {
             try {
                 Object object = in.readObject();
                 if(object instanceof ChooseSubjectObject){
-                    List<String> threeSubjects = ((ChooseSubjectObject) object).subjects;
+                    threeSubjects = ((ChooseSubjectObject) object).subjects;
                     System.out.println("Client el subjects");
+//                    Platform.runLater(() -> quizClient.goToResultScene());
                     Platform.runLater(() -> quizClient.goToChoseSubjectScene(threeSubjects)); // main thread runs this when it has time.
                 }
                 else if(object instanceof QuestionsBySubjectObject){
@@ -90,8 +94,8 @@ public class ServerConnection extends Thread {
                     Map<String, Integer> stats = (Map<String, Integer>) object;
                     String firstKey = stats.keySet().stream().findFirst().get();
                     int firstValue = stats.get(firstKey);
-                    Platform.runLater(() -> quizClient.getResultScene().getPlayerTwoText().setText(firstKey) ); // Tillfällig
-                    Platform.runLater(()-> quizClient.getResultScene().getRoundOneResult2().setText(firstValue + "/x")); // Tillfällig
+                    quizClient.getResultScene().getPlayerTwoText().setText(firstKey);// Tillfällig
+                    quizClient.getResultScene().getRoundOneResult2().setText(firstValue + "/x"); // Tillfällig
                     Platform.runLater(() -> quizClient.goToResultScene());
                 }
             } catch (ClassNotFoundException | IOException e) {
--- src/com/quizgame/server/Game.java
+++ src/com/quizgame/server/Game.java
@@ -49,7 +49,9 @@ public class Game {
         threeSubjects.add(database.getSubjectList().get(0));
         threeSubjects.add(database.getSubjectList().get(1));
         threeSubjects.add(database.getSubjectList().get(2));
-        player2.sendSubjectsToClient(threeSubjects);
+        if() {
+            player2.sendSubjectsToClient(threeSubjects);
+        }
     }
     public List<QuizItem> getQuestionsBySubjects(String subject){
         return database.getItemsBySubject(subject);
